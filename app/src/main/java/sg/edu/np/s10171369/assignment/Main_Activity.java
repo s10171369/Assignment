dd7fc87ef8bff91f072b89878d99c4517ca16eca
Hsien Wei Joachim Lee <s10171146@connect.np.edu.sg>
2018-07-10 12:44:14 +0800

    Testing

--- app/src/main/java/sg/edu/np/s10171369/assignment/Main_Activity.java
+++ app/src/main/java/sg/edu/np/s10171369/assignment/Main_Activity.java
@@ -9,5 +9,7 @@ public class Main_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_);
+
+        //Testing one two three
     }
 }
