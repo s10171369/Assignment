ba55925273fa4c6fad69596d09d52960394bb926
Hsien Wei Joachim Lee <s10171146@connect.np.edu.sg>
2018-07-11 12:02:50 +0800

    Pages Addition

---
+++ app/src/main/java/sg/edu/np/s10171369/assignment/HeroGuide.java
@@ -0,0 +1,13 @@
+package sg.edu.np.s10171369.assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeroGuide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hero_guide);
+    }
+}
---
+++ app/src/main/java/sg/edu/np/s10171369/assignment/ItemListGuide.java
@@ -0,0 +1,13 @@
+package sg.edu.np.s10171369.assignment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemListGuide extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_list_guide);
+    }
+}
