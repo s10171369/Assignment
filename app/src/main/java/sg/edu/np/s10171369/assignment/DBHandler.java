8a8680b0781f43d815fb96e555ca14162c4540c3
Hsien Wei Joachim Lee <s10171146@connect.np.edu.sg>
2018-07-22 21:44:06 +0800

    DB Addition

---
+++ app/src/main/java/sg/edu/np/s10171369/assignment/DBHandler.java
@@ -0,0 +1,9 @@
+package sg.edu.np.s10171369.assignment;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHandler extends SQLiteOpenHelper {
+    
+}
