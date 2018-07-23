3db009ffa757092d877b68f3ef43e99f2c47ba03
Hsien Wei Joachim Lee <s10171146@connect.np.edu.sg>
2018-07-23 17:38:15 +0800

    DataHandler

--- app/src/main/java/sg/edu/np/s10171369/assignment/DBHandler.java
+++ app/src/main/java/sg/edu/np/s10171369/assignment/DBHandler.java
@@ -4,5 +4,9 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DBHandler {//extends SQLiteOpenHelper {
+public class DBHandler extends SQLiteOpenHelper {
+    private static String DB_NAME = "DBMAD_Assignment.db";
+    private static String DB_PATH = "";
+    //C:UsersjoahAndroidStudioProjectsAssignmentappsrcmainres
+    private static final int DB_VERSION = 1;
 }
